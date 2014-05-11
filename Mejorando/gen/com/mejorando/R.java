f4994d79893e8fea11d3336f6a778eb47f25083e
ncoap <aries_2221@hotmail.com>
2014-05-11 15:25:11 -0500

    Image view mas otros elemntos
    toggle, un spinner

--- Mejorando/gen/com/mejorando/R.java
+++ Mejorando/gen/com/mejorando/R.java
@@ -23,10 +23,11 @@ public final class R {
         public static final int rating_not_important=0x7f020005;
     }
     public static final class id {
-        public static final int action_dialog=0x7f080003;
-        public static final int action_fav=0x7f080001;
-        public static final int action_share=0x7f080002;
+        public static final int action_dialog=0x7f080004;
+        public static final int action_fav=0x7f080002;
+        public static final int action_share=0x7f080003;
         public static final int imageView1=0x7f080000;
+        public static final int toggleButton1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
